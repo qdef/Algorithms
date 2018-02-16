432bb789f29acd13f191e5e9e47134ce34e81770
qdef <quentin.defenouillere@gmail.com>
2018-02-16 13:09:21 +0100

    whatevz

--- List Sorting Algo.py
+++ List Sorting Algo.py
@@ -4,8 +4,12 @@ input_list = [87, 45, 12, 5, 47, 85, 12, 16, 51, 57, 74, 43, 17, 55, 2, 0, 1, 19
 def sorting(x):
 	sorted_list=[]
 	total=len(x)
+	# Instead of iterating through the input list, I use a while loop that breaks when the output list has the length of the original input list.
+	# This enables the deletion of elements from the input list, which could not be done while iterating through it.
 	while len(sorted_list)<total:
+		# Adding the smallest element to the output list:
 		sorted_list.append(min(x))
+		# Removing the added element from the input list:
 		x.remove(min(x))
 	return sorted_list
 
