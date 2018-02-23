a1e83f0e0b6803d4fb09fbb60f6d9fcbba43385c
qdef <quentin.defenouillere@gmail.com>
2018-02-23 17:47:39 +0100

    whatevz

--- Ronronnement.py
+++ Ronronnement.py
@@ -1,26 +1,15 @@
-#*******
-#* Read input from STDIN
-#* Use echo or print to output your result, use the /n constant at the end of each result line.
-#* Use:
-#*      local_print (variable ); 
-#* to display simple variables in a dedicated area.
-#* ***/
 import sys
+#lines = []
+#for line in sys.stdin:
+	#lines.append(line.rstrip('\n'))
 
-lines = []
-for line in sys.stdin:
-	lines.append(line.rstrip('\n'))
+#Example:
+lines = ['3284493037959832844930379598']
 
 ronron=lines[0]
-local_print(ronron)
 N=len(ronron)
-
-pattern_list=[]
 for i in range(1, N+1):
-	if N%i==0:
-		pattern = ronron[0:i]
-		local_print(pattern*int(N/i))
-		if pattern*int(N/i)==ronron:
+	if N%i==0 and ronron[0:i]*int(N/i)==ronron:
 			print(i)
 			break
 
