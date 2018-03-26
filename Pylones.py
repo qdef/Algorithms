a59f3c66f0209b16a4536ea6760ed1d40de9aa7d
qdef <quentin.defenouillere@gmail.com>
2018-03-26 12:22:38 +0200

    changed indentation basis

--- Pylones.py
+++ Pylones.py
@@ -7,40 +7,41 @@
 #*      local_print (variable ); 
 #* to display simple variables in a dedicated area.
 #* ***/
+
 import sys
 
 lines = []
 for line in sys.stdin:
-        lines.append(line.rstrip('\n'))
+    lines.append(line.rstrip('\n'))
 
 pylones = []
 
 for i in lines[1:]:
-        pylones.append(int(i))
+    pylones.append(int(i))
 
 
 def vision(L, x):
-        total = 0
-        if x > 0:
+    total = 0
+    if x > 0:
+        total+=1
+        maxi = L[x-1]
+        for j in L[x-1::-1]:
+            if j > maxi:
                 total+=1
-                maxi = L[x-1]
-                for j in L[x-1::-1]:
-                        if j > maxi:
-                                total+=1
-                                maxi = j
-        if x < len(L)-1:
+                maxi = j
+    if x < len(L)-1:
+        total+=1
+        maxi = L[x+1]
+        for i in L[x+1:]:
+            if i > maxi:
                 total+=1
-                maxi = L[x+1]
-                for i in L[x+1:]:
-                        if i > maxi:
-                                total+=1
-                                maxi = i
-        return total
+                maxi = i
+    return total
 
 output=''
 
 for e, i in enumerate(pylones):
-        height = vision(pylones, e)
-        output+=' ' + str(height)
+    height = vision(pylones, e)
+    output+=' ' + str(height)
 
 print(output)
